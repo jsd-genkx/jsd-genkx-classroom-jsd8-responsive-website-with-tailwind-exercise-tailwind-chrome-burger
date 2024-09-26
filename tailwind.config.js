9c6d1e2f898528f2c5e76a2fcb64d38e1e0cba63
Virachai Wongsena <wvirachai@gmail.com>
2024-09-26 12:57:55 +0700

    add license file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./components/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./index.html",
+  ],
   theme: {
     extend: {},
   },
